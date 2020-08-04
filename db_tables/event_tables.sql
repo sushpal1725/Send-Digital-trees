67ede933e3560587b1f3f46604b56f6eb02d82a8
sushmitha palleti <64373987+sushpal1725@users.noreply.github.com>
2020-08-04 10:56:09 -0500

    Add files via upload

---
+++ db_tables/event_tables.sql
@@ -0,0 +1,33 @@
+#Creating SQL table for each event category 
+
+CREATE TABLE registrations (
+day TEXT,
+my_date DATE,
+my_time TIME,
+user_id INTEGER,
+event_type TEXT,
+birth_year INTEGER,
+phone_type TEXT,
+location TEXT,
+source TEXT);
+Putting data into the registrations SQL table:
+
+COPY registrations FROM '/home/sush/all_registrations.txt' DELIMITER ' ';
+
+CREATE TABLE super_tree (
+day TEXT,
+my_date DATE,
+user_id INTEGER,
+event_type TEXT);
+Putting data into the super_tree SQL table:
+
+COPY super_tree FROM '/home/sush/all_super_trees.txt' DELIMITER ' ';
+
+CREATE TABLE free_tree (
+day TEXT,
+my_date DATE,
+user_id INTEGER,
+event_type TEXT);
+Putting data into the free_tree SQL table:
+
+COPY free_tree FROM '/home/sush/all_free_trees.txt' DELIMITER ' ';
