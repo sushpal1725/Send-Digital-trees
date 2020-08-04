50bc1a18ac4e2276f13e9c3cd6c1669ef88e69f3
sushmitha palleti <64373987+sushpal1725@users.noreply.github.com>
2020-08-04 11:08:30 -0500

    Add files via upload

---
+++ db_tables/event_tables/csv_filter (1).sh
@@ -0,0 +1,4 @@
+###CSV files for each event type 
+cat * | sort -V > /home/sush/log/event_based_csv/all_registrations.csv
+cat * | sort -V > /home/sush/log/event_based_csv/all_free_tree.csv
+cat * | sort -V > /home/sush/log/event_based_csv/all_super_tree.csv
