2bae5bcef76b247668f3fec5a8b270636cca5d14
sushmitha palleti <64373987+sushpal1725@users.noreply.github.com>
2020-08-06 13:19:08 -0500

    Update daily_revenue.sql

--- segmentation/kpis/daily_revenue.sql
+++ segmentation/kpis/daily_revenue.sql
@@ -7,3 +7,8 @@ WHERE my_date < (SELECT current_date as today ))
 (SELECT COUNT(*)-COUNT(DISTINCT(user_id))
 FROM super_tree
 WHERE my_date < (SELECT current_date-1 as yesterday))AS kpi;
+-----------------------------------------------------------------
+#daily revenue throughout lifecycle
+SELECT my_date, COUNT(*) -COUNT(DISTINCT(user_id))
+FROM super_tree
+GROUP BY my_date;
